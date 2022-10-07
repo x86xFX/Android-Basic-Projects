0662a16f32169e0d4cd66b6acae283b1a720a743
Theekshana Nirmana <theek1010@gmail.com>
2022-10-07 16:56:26 +0530

    Update MainFragment.kt

--- ViewBindingProject/app/src/main/java/com/basic/viewbindingproject/MainFragment.kt
+++ ViewBindingProject/app/src/main/java/com/basic/viewbindingproject/MainFragment.kt
@@ -4,11 +4,11 @@ import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+
 import com.basic.viewbindingproject.databinding.FragmentMainBinding
 
 class MainFragment : Fragment(R.layout.fragment_main) {
     private lateinit var binding: FragmentMainBinding
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
