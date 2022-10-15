c55dd939435fc6ba518fd96a6c032218024d829e
Theekshana Nirmana <theek1010@gmail.com>
2022-10-15 13:51:51 +0530

    Navigation Drawer

--- NavigationDrawer/app/src/main/java/com/basic/navigationdrawer/MainActivity.kt
+++ NavigationDrawer/app/src/main/java/com/basic/navigationdrawer/MainActivity.kt
@@ -2,14 +2,39 @@ package com.basic.navigationdrawer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import com.basic.navigationdrawer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        toggle = ActionBarDrawerToggle(this, binding.drawerLayout, R.string.open, R.string.close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.miGroup -> Toast.makeText(this, "New Group Clicked", Toast.LENGTH_SHORT).show()
+                R.id.miLocation -> Toast.makeText(this, "Location Clicked", Toast.LENGTH_SHORT).show()
+                R.id.miContact -> Toast.makeText(this, "Contacts Clicked", Toast.LENGTH_SHORT).show()
+                R.id.miCall -> Toast.makeText(this, "Calls Clicked", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){ return true }
+        return super.onOptionsItemSelected(item)
     }
 }
